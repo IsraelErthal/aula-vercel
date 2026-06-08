a87b16f01d478c87d7b70797cb52d4dbfb8da5f2
Israel Erthal <erthalisrael77@gmail.com>
2026-06-08 19:47:02 -0300

    feature: página criada

---
+++ minha-vercelandia/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+   <div>
+    <h1>Seja bem vindo a Vercelândia</h1>
+    <h2>Me chame de Rael e hoje nós vamos Vercelar!!!!!!</h2>
+   </div>
+  );
+}
+
+export default App;
+  
